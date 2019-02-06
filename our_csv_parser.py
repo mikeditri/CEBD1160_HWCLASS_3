be993d49603ccd5e64c6164554ec0ca8f9031e65
mikeditri <mikeditri@hotmail.com>
2019-02-06 14:45:53 -0500

    script can now change each item to float or int

--- our_csv_parser.py
+++ our_csv_parser.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 
-#import os
-#
-#
-#if os.path.isfile(myfilename):
-#    print('yay, I have a file')
-#else:
-#    print('Boo, I dont have')
+import os
 
 myfilename = "housing.data.txt"
 
 with open(myfilename, 'r') as file_handle:
     for line in file_handle.readlines():
-        line_clean = line.replace('   ',' ').replace('  ',' ')
+        line_clean = line.replace('   ', ' ').replace('  ', ' ')
         line_clean = line_clean.strip()
         values = line_clean.split(' ')
-        print(values)
-        for value in values:
-            print(float(value))
-        
-    print('Finished')
+        #print(values)
+        value= [float,float,float,int,float,float,float,float,int,float,float,float,float,float] 
+        newlist = [t(x) for t,x in zip(value,values)]
+        print(newlist)    
+
+       
+#HOLY MOLY IT WORKED!!!
+
+       
+    # for homework:
+            # identify what type of data each value is, and cast it
+            # to the appropriate type, then print the new, properly-typed
+            # list to the screen.
+            # I.e. ['0.04741', '0.00', '11.930', '0', '0.5730', '6.0300', '80.80', '2.5050', '1', '273.0', '21.00', '396.90', '7.88', '11.90']
+            # becomes: [0.04741, 0.0, 11.93, 0, 0.573, '6.03, 80.8, 2.505, 1, 273.0, 21.0, 396.90, 7.88, 11.90]
